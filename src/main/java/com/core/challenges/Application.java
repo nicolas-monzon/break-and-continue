cbeb6a0b25a79ea67b881079d0b35dc77fe3ce04
Nicolás A. Monzón <nmonzon.2022@gmail.com>
2023-01-01 23:38:36 -0300

    Main

--- src/main/java/com/core/challenges/Application.java
+++ src/main/java/com/core/challenges/Application.java
@@ -2,12 +2,16 @@ package com.core.challenges;
 
 import com.core.challenges.challenges._01.DivisionAlgorithm;
 import com.core.challenges.challenges._02.InputAndOutput;
+import com.core.challenges.challenges._03.GenericProblems;
+import com.core.challenges.challenges._04.Homework;
 
 public class Application {
 
     public static void main(String[] args) {
         DivisionAlgorithm divisionAlgorithm = new DivisionAlgorithm();
         InputAndOutput inputAndOutput = new InputAndOutput();
+        GenericProblems genericProblems = new GenericProblems();
+        Homework homework = new Homework();
     }
 
 }
